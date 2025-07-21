e4ebffa146724372a7417105419dd81fe6ce6671
Павел <72077736+pasha9107@users.noreply.github.com>
2025-07-21 16:15:57 +0300

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-# Добавляем корневую директорию проекта в sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.database import Base
